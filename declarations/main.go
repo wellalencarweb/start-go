24fb7ec5b24899cac931e8303773efd0ac6d0cf8
wellalencarweb <wellingtonlogia@gmail.com>
2024-11-07 08:29:19 -0300

    feat: :rocket: start code in go

---
+++ declarations/main.go
@@ -0,0 +1,18 @@
+package main
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "Tom"
+	e float64 = 35.11
+)
+
+func main() {
+	a := "First atribuition"
+	println(a)
+
+	a = "Second atribuition"
+	println(a)
+
+	println(b, c, d, e)
+}
