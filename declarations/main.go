32cf3dd7406a05207cce9f0f787a8097c3dda8e4
wellalencarweb <wellingtonlogia@gmail.com>
2024-11-11 09:21:58 -0300

    feat: declarations and atribuitions

--- declarations/main.go
+++ declarations/main.go
@@ -1,10 +1,17 @@
 package main
 
+/*importation packeges*/
+import "fmt"
+
+/*create types example*/
+type ID int
+
 var (
 	b bool    = true
 	c int     = 10
 	d string  = "Tom"
 	e float64 = 35.11
+	f ID      = 1
 )
 
 func main() {
@@ -15,4 +22,10 @@ func main() {
 	println(a)
 
 	println(b, c, d, e)
+
+	//print type creted
+	println(f)
+
+	//print use packege fmt type and value
+	fmt.Printf("O Tipo de E é: %T, O Valor de E é:%v\n", e, e)
 }
