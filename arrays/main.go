a29a8a51ef9c09f9bd021933a9cfe289f1b1c153
wellalencarweb <wellingtonlogia@gmail.com>
2024-11-24 09:41:58 -0300

    feat: context to http

--- arrays/main.go
+++ arrays/main.go
@@ -15,4 +15,12 @@ func main() {
 	for i, v := range myArray {
 		fmt.Printf("O Valor do indice é %d  e o valor é %d\n", i, v)
 	}
+
+	//Declaring array with values
+	myArray2 := [3]int{50, 60, 70}
+	fmt.Println(myArray2)
+
+	//Declaring multidimensional array
+	myArray3 := [3][2]int{{10, 20}, {30, 40}, {50, 60}}
+	fmt.Println(myArray3)
 }
---
+++ http-context/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://google.com", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	println(string(body))
+}
